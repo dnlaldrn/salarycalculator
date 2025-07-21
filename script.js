52dfa0f00c0b6b100ec97282e78376b6a02631a7
Daniel Aldreen Manjares <danielaldreenm@gmail.com>
2025-07-21 11:13:22 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
- calculateregHours = () =>{
+calculateTotalIncome = () =>{
+          const regInput = document.querySelector('.regularHours').value.trim();
+            const otInput = document.querySelector('.OThours').value.trim();
+
+         if(regInput === " "&& otInput === ""){
+            document.querySelector('.display').innerHTML = "Invalid Input ";
+            }else{
+                const regincome = calculateregHours();
+                const otincome = calculateOTHours();
+                const totalIncome = regincome + otincome;
+
+calculateregHours = () =>{
         const regularHours = document.querySelector('.regularHours').value
         const convertedRegHours = regularHours * 67.5;
         return convertedRegHours;
@@ -12,26 +23,39 @@
      }
      calculateTotalIncome = () =>{
           const regInput = document.querySelector('.regularHours').value.trim();
-            const otInput = document.querySelector('.OThours').value.trim();
+          const otInput = document.querySelector('.OThours').value.trim();
+          const regincome = calculateregHours();
+          const otincome = calculateOTHours();
+          const totalIncome = regincome + otincome;
+          const incomesssdeducted = totalIncome *.05;
+          const philhealthdeducted = ((totalIncome * 2)* .05)/2;
+          const pagibigdeducted = totalIncome * .02;
+          const totaldeduction = incomesssdeducted + philhealthdeducted + pagibigdeducted;
+          const netIncome =totalIncome -totaldeduction;
 
-         if(regInput === " "&& otInput === ""){
-            document.querySelector('.display').innerHTML = "Invalid Input ";
-            }else{
-                const regincome = calculateregHours();
-                const otincome = calculateOTHours();
-                const totalIncome = regincome + otincome;
-                const incomesssdeducted = totalIncome *.05;
-                const philhealthdeducted = ((totalIncome * 2)* .05)/2;
-                const pagibigdeducted = totalIncome * .02;
-                const totaldeduction = incomesssdeducted + philhealthdeducted + pagibigdeducted;
-                const netIncome =totalIncome -totaldeduction;
+        if(netIncome > 5500){
+             document.querySelector('.display').innerHTML = `
+                <p>Total Income: ${totalIncome.toFixed(2)}</p>
+                <p>SSS : ${incomesssdeducted.toFixed(2)}</p>
+                <p> PhilHealth: ${philhealthdeducted.toFixed(2)}</p>
+                <p>Pagibig : ${pagibigdeducted.toFixed(2)}</p>
+                <p> Net Income: ${netIncome.toFixed(2)}</p>
+                <p>Nakss Paldoo</p>`
+
+        }
+            else{
                 document.querySelector('.display').innerHTML = `
                 <p>Total Income: ${totalIncome.toFixed(2)}</p>
                 <p>SSS : ${incomesssdeducted.toFixed(2)}</p>
                 <p> PhilHealth: ${philhealthdeducted.toFixed(2)}</p>
                 <p>Pagibig : ${pagibigdeducted.toFixed(2)}</p>
-                <p> Net Income: ${netIncome.toFixed(2)}</p>`
+                <p> Net Income: ${netIncome.toFixed(2)}</p>
+                 <p>Konting Extend pa boss</p>`
+               
+                
             }
+            
+           
         
            
      }
